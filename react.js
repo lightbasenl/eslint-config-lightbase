1692d6b37d0a60b2c7b329a277779a90c8fdea10
Oliver <oliver@lightbase.nl>
2019-05-24 11:17:19 +0200

    add curly error rules to react

--- react.js
+++ react.js
@@ -1,27 +1,8 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "react-app",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-    "prettier/react",
-  ],
-  plugins: ["@typescript-eslint", "prettier", "react", "react-hooks"],
-  settings: {
-    react: {
-      pragma: "React",
-      version: "detect",
-    },
-  },
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
+  extends: ["react-app", "plugin:prettier/recommended"],
   rules: {
     ...require("./lib/rules"),
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    curly: "error",
   },
 };
