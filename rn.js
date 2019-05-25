b508d52a98c386a3486825c2fdb21ea65e033b46
Oliver <oliver@lightbase.nl>
2019-05-25 17:34:07 +0200

    Move typescript rules to overrides in RN

--- rn.js
+++ rn.js
@@ -1,34 +1,42 @@
-module.exports = {
-  extends: ["@react-native-community", "plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
+{
+  "extends": ["@react-native-community", "plugin:prettier/recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
-  rules: {
-    ...require("./lib/rules"),
-    "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/member-delimiter-style": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/type-annotation-spacing": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-        ignoreRestSiblings: false,
-      },
-    ],
-    "default-case": ["warn", { commentPattern: "^no default$" }],
+  "overrides": [
+    {
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        ...require("./lib/rules"),
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/explicit-member-accessibility": "off",
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/member-delimiter-style": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/type-annotation-spacing": "off",
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          {
+            vars: "all",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+            ignoreRestSiblings: false,
+          },
+        ],
+      }
+    }
+  ],
+  "rules": {
+    "default-case": ["warn", { "commentPattern": "^no default$" }],
     "no-nested-ternary": "warn",
     "no-unneeded-ternary": "warn",
     "no-useless-return": "warn",
     "prefer-destructuring": "warn",
     "react/no-unescaped-entities": "off",
-    "react/jsx-curly-brace-presence": ["error", { props: "never", children: "never" }],
+    "react/jsx-curly-brace-presence": ["error", { "props": "never", "children": "never" }],
     "react/no-array-index-key": "warn",
-    "react/no-unused-state": "error",
-  },
-};
+    "react/no-unused-state": "error"
+  }
+}
